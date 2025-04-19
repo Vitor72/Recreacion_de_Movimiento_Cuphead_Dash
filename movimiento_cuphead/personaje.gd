cca89a91edbc128cc000a54dcf475f1e7c44fb37
Victor 72 <coronelvictor651@gmail.com>
2025-04-19 17:42:22 -0300

    Sprites del nivel de prueba y ajuste de los parametros del player

--- movimiento_cuphead/personaje.gd
+++ movimiento_cuphead/personaje.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
-var speed : int = 150
-const JUMP_VELOCITY : int = -400.0
+var speed : int = 200
+const JUMP_VELOCITY : int = -550.0
 var gravity : int = 1200
 var dash : bool = true
 var maximo_dash : int = 2
@@ -21,13 +21,16 @@ func _physics_process(delta):
 		elif direction == -1:
 			$Animaciones.flip_h = true
 	else:
+		##velocity.x = 0
 		velocity.x = move_toward(velocity.x, 0, speed)
-		
 	if Input.is_action_just_pressed("Dash"):
 		if dash == true:
-			speed = 250 + 500
-			$Dash_on.start(0.2)
+			speed = 200 + 350
+			$Animaciones.animation = "dash"
+			$Animaciones.play("dash")
+			$Dash_on.start(0.3)
 			dash = false
+
 
 	if is_on_floor():
 		if velocity.x != 0:
@@ -37,17 +40,14 @@ func _physics_process(delta):
 		if velocity.y <= -1:
 			$Animaciones.animation = "Jump"
 			$Animaciones.play("Jump")
-		else:
-			$Animaciones.animation = "Fall"
-			$Animaciones.play("Fall")
-	if velocity.x == 0 && velocity.y == 0:
+	if velocity.x == 0 && is_on_floor():
 		$Animaciones.play("Idle")
 		
 	move_and_slide()
 
 func _on_dash_on_timeout():
-	speed = 250
-	$Cooldawn_Dash.start(2)
+	speed = 200
+	$Cooldawn_Dash.start(0.5)
 	$Dash_on.stop()
 	pass # Replace with function body.
 
